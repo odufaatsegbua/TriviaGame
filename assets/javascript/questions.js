92dedbfaaa2ccfa9dce96370a0cd396b919cf3da
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-09-26 16:57:55 -0500

    adding html and javascripts

---
+++ assets/javascript/questions.js
@@ -0,0 +1,29 @@
+cont gameQuestions = [
+    {
+        questions: "What month was Drake born?",
+        choices: ["July", "Novemeber", "October", "December"],
+        correctAnswer: "October",
+    },
+
+    {
+        questions: "What is Drake's real name?",
+        choices: ["Drake", "Graham", "Jimmy", "Aubrey"],
+        correctAnswer: "Aubrey",
+    },
+
+    {
+        questions: "What year was Take Care released?",
+        choices: ["2009", "2011", "2013", "2010"],
+        correctAnswer: "2011",
+    },
+
+    {
+        questions: "What was Drake first album?",
+        choices: ["Take Care", "So Far Gone", "Thank Me Later", "Best I Ever Had"],
+        correctAnswer: "So Far Gone",
+    },
+
+
+
+]
+
