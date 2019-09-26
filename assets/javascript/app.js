fcbf4d592524d860c611dc843f57eaf6bb6eaa79
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-09-26 17:51:39 -0500

    updating changes load questions says undefined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,19 @@
-console.log("App");
+// // game variables \\
+
+// let counter = 30;
+// let currentQuestion = 0;
+// let score = 0;
+// // let wins = 0;
+// let loss = 0;
+// let timer;
+
+// // This function will display the questions and answers choices to the browser\\
+ 
+// function loadQuestion() {
+//     const question = gameQuestions[currentQuestion].question;
+//     const choices = gameQuestions[currentQuestion].choices;
+
+//     $("#game").html("<h4>" + question + "</h4>");
+// }
+
+// loadQuestion();
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,4 +1,7 @@
-cont gameQuestions = [
+
+// Create an array of the game questions and answers\\
+
+const gameQuestions = [
     {
         questions: "What month was Drake born?",
         choices: ["July", "Novemeber", "October", "December"],
@@ -23,7 +26,26 @@ cont gameQuestions = [
         correctAnswer: "So Far Gone",
     },
 
+];
 
 
-]
+// game variables \\
 
+let counter = 30;
+let currentQuestion = 0;
+let score = 0;
+// let wins = 0;
+let loss = 0;
+let timer;
+
+// This function will display the questions and answers choices to the browser\\
+
+//  var loadQuestion = 
+function loadQuestion() {
+    const question = gameQuestions[currentQuestion].question;
+    const choices = gameQuestions[currentQuestion].choices;
+
+    $("#game").html("<h4>" + question + "</h4>");
+}
+
+loadQuestion();
